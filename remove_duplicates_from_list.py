1c1071ba11b1b3d1c8c0deb81241103ba4b20282
a2623212 <a2623212@gmail.com>
2024-01-05 10:44:54 +0800

    feat: id44 completed

--- remove_duplicates_from_list.py
+++ remove_duplicates_from_list.py
@@ -1,2 +1,8 @@
 def distinct(seq):
-    pass
+    seen = []
+    first_seen = []
+    for item in seq:
+        if item not in seen:
+            seen.append(item)
+            first_seen.append(item)
+    return first_seen
