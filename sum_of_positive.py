e157c6afb1003c096e770fa046c314cfdee51d7e
a2623212 <a2623212@gmail.com>
2024-01-05 23:36:32 +0800

    feat: id41 completed

--- sum_of_positive.py
+++ sum_of_positive.py
@@ -1,2 +1,11 @@
 def positive_sum(arr):
-    pass
+    if type(arr) is not list:
+        return
+
+    positive_num = []
+    for i in arr:
+        if type(i) is int and i >= 0:
+            positive_num.append(i)
+
+    return sum(positive_num)
+
